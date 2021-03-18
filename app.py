4f5ca1c494ebaa66f873505cd71e9f866af7e642
Marisa <mpapagel@wellesley.edu>
2021-03-18 12:26:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,33 @@ def jobs(comp_id):
 
     return render_template('jobs.html', comp_id=comp_id, name=comp_name, jobs=jobs_iterate)
 
+@app.route('/affiliate/<username>', methods=['GET', 'POST'])
+def affiliate(username):
+    #Set up connection.
+    conn = dbi.connect()
+    #Create cursor to pull data from the user table.
+    curs = dbi.dict_cursor(conn)
+    curs.execute("select * from user where username = %s", [username])
+    usernow = curs.fetchone()
+    #Assign variables.
+    user_name = usernow['name']
+    userid = usernow['username']
+    major = usernow['major']
+    gpa = usernow['GPA']
+    Org1= usernow['org1']
+    Org2= usernow['org1']
+    Org3= usernow['org1']
+    if request.method = 'GET':
+        return render_template('affiliate-page.html',  name=user_name, 
+                                username=userid, org1=org1, org2=org2,org3=org3,
+                                description=None, reps=reps_iterate)
+    else: 
+        return redirect(url_for('affiliate_update'), username=username)
 
+@app.route('/affiliate/<username>/update', methods=['GET', 'POST'])
+def affiliate_update(username):
+    
+    
     
 @app.before_first_request
 def init_db():
