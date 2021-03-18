71fc41546d519b75be96077ee6586479708fd9f4
luizaeboli <lmiranda@wellesley.edu>
2021-03-18 11:58:33 -0400

    Update affiliate.py

--- affiliate.py
+++ affiliate.py
@@ -1,25 +1,28 @@
 #very rough version of py file, because we need to change our tables :) 
 
-import cs304dbi as dbi # is this what we import?
-    
-def get_person(conn,username):
-    '''Returns the name, gpa, major, org1,org2,org3,industry for the user"
-    '''
+import cs304dbi as dbi 
 
-    curs = dbi.dict_cursor(conn)
-    curs.execute('''select user.name, user.gpa , user.major, user.org1, user.org2,user.org3, user.industry from user where person.nm=%s''', [username]) 
-    # the queries will change if we change our tables to only affiliates or do we include everything in user? We need to discuss this
-    return curs.fetchone()
+import random
 
-def get_experience(conn,jobid):
-    '''Returns a dictionary with a list of jobs and job ids  of jobs a specific person has worked for " '''
-    curs = dbi.dict_cursor(conn)
-    #is the person including the title or the user id for their job, confused how we are going to do this. 
-    curs.execute('''select job.title,job.jobid,  where jobid=%s''',[jobid])
-    return curs.fetchall()
-
-
-if __name__ == '__main__':
-    dbi.cache_cnf()   # defaults to ~/.my.cnf
-    dbi.use('ourdb') #what is our db called? 
+@app.route('/affiliate/<username>', methods=['GET', 'POST'])
+def affiliate(username):
+    #Set up connection.
     conn = dbi.connect()
+    #Create cursor to pull data from the user table.
+    curs = dbi.dict_cursor(conn)
+    curs.execute("select * from user where username = %s", [username])
+    usernow = curs.fetchone()
+    #Assign variables.
+    user_name = usernow['name']
+    userid = usernow['username']
+    major = usernow['major']
+    gpa = usernow['GPA']
+    Org1= usernow['org1']
+    Org2= usernow['org1']
+    Org3= usernow['org1']
+    if request.method == 'GET':
+        return render_template('affiliate-page.html',  name=user_name, 
+                                username=userid, org1=org1, org2=org2,org3=org3,
+                                description=None, reps=reps_iterate)
+    else:
+        return redirect(url_for('jobs', comp_id=comp_id))
