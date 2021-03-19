34135f20d51efe9ec32a714cfba53d81473a0a72
luizaeboli <lmiranda@wellesley.edu>
2021-03-19 11:47:07 -0400

    Update affiliate.py

--- affiliate.py
+++ affiliate.py
@@ -3,13 +3,18 @@ import cs304dbi as dbi
 def get_affiliate(conn,username):
    #Set up connection.
     conn = dbi.connect()
+    #Create cursor to pull data from the user table
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''select welles_affiliates.username, welles_affiliates.year, welles_affiliates.major, welles_affiliates.gpa, welles_affiliates.org1, 
+                     welles_affiliates.org2,welles_affiliates.org3 where username=%s''' [username])
+    return curs.fetchone()
+
+def get_experience(conn,username): 
+    conn = dbi.connect()
     #Create cursor to pull data from the user table.
     curs = dbi.dict_cursor(conn)
-    curs.execute("select welles_affiliates.username as 'username', welles_affiliates.year as 'year', welles_affiliates.major as 'major', welles_affiliates.gpa as 'gpa', welles_affiliates.org1 as 'org1', 
-                     welles_affiliates.org2 as 'org2',welles_affiliates.org3 as 'org3', worked_for.comp_id as 'comp_id', company.comp_name as 'comp_name'
-                     from welles_affiliates inner join worked_for using(username)
-                     inner join company using (comp_id)")
-    return curs.fetchone()
+    curs.execute(''' select experience.username, experience.jid, experience.comp_id, company.comp_name from experience left outer join company where username=%s''' [username]
+
 
 
  
