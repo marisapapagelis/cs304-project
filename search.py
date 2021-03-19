04c588bd1b9e655614add5ee1e73c76f0c4233e7
luizaeboli <lmiranda@wellesley.edu>
2021-03-19 12:41:40 -0400

    Update search.py

--- search.py
+++ search.py
@@ -1,13 +1,36 @@
 
-@app.route('/search/')
-def search():
-    kind = request.args['kind']
+@app.route('/search/', methods = ['GET'])
+def query():
     industry = request.args['industry']
-
-    conn = dbi.connect()
-    curs = dbi.cursor(conn)
-
-    if kind == ''
+    kind= request.args['kind']
+    conn=dbi.connect()
+    if kind =='company':
+        companylist=company.get_company(conn,comp_id)
+        if len(companylist) == 1: 
+            return redirect(url_for('company', comp_id=companylist[0]['comp_id']))
+        elif len(companylist) > 1: 
+            return render_template('company-list.html',companylist=companylist,kind=kind)
+        else :
+            flash('Sorry, no company with this name exists.')
+            return redirect(url_for('index'))
+    elif kind == 'industry':
+        industrylist=industry.get_industry(conn,comp_id)
+        if len(industrylist) == 1: 
+            return redirect(url_for('industry', iid=industrylist[0]['iid']))
+        elif len(industrylist) > 1: 
+            return render_template('industry-list.html',industrylist=industrylist,kind=kind)
+        else :
+            flash('Sorry, no industry with this name exists.')
+            return redirect(url_for('index'))
+    else: 
+        personlist=affiliate.get_affiliate(conn,username)
+        if len(personlist) == 1: 
+            return redirect(url_for('affiliate',username=personlist[0]['username']))
+        elif len(personlist) > 1: 
+            return render_template('affiliate-list.html',personlist=personlist,kind=kind)
+        else:
+            flash('Sorry, no company with this name exists.')
+            return redirect(url_for('index'))
 
 
 
