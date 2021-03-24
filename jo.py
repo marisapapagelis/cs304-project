9f5af139bdbe8b0a93965e96adadf0341c3e28b9
Nina Goodman <ninakgoodc@gmail.com>
2021-03-24 13:17:38 -0400

    Update jo.py

--- jo.py
+++ jo.py
@@ -11,9 +11,9 @@ def get_jobs(conn,comp_id):
     jobs.qual2, jobs.qual3, jobs.job_status,jobs.app_link from company inner join jobs using (comp_id) where comp_id = %s''', [comp_id])
     return curs.fetchall()
 
-def get_job(jid):
+def get_job(conn,jid):
     conn = dbi.connect()
     curs = dbi.dict_cursor(conn)
-    curs.execute('''select title,jid from jobs where jid=%s''',[jid])
+    curs.execute('''select * from jobs where jid=%s''',[jid])
     return curs.fetchone()
   
