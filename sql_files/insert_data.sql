67bfd9322fa7297362ae6e17f30f678e735f08bc
Nina Goodman <ninakgoodc@gmail.com>
2021-03-24 20:47:04 -0400

    added some data

--- sql_files/insert_data.sql
+++ sql_files/insert_data.sql
@@ -17,6 +17,21 @@ VALUES ('lu1','Luiza', 'monkey657', 'lmiranda');
 insert into user(username, name, passwd, email) 
 values ('ng', 'Nina', 'puppy', 'ngoodman');
 
+insert into user(username, name, passwd, email) 
+values ('styles', 'Harry Styles', '1dforever', 'hstyles@1dforever.com');
+
+insert into user(username, name, passwd, email) 
+values ('horan', 'Niall Horan', 'im_irish', 'nhoran@1dforever.com');
+
+insert into user(username, name, passwd, email) 
+values ('louis', 'Louis Tomlinson', '1d_rox', 'louis@1dforever.com');
+
+insert into user(username, name, passwd, email) 
+values ('liam', 'Liam Payne', 'no_payne_no_gain', 'liam@1dforever.com');
+
+insert into user(username, name, passwd, email) 
+values ('zayn', 'Zayn Malik', 'drive_u_in_zayn', 'zayn@1dforever.com');
+
 -- insert wellesley affiliates into wellesley affiliate table
 INSERT INTO welles_affiliates (username, year, major, gpa, org1, org2, org3)
 VALUES ('mars', 2022, 'Economics', 3.98, 'CSClub', 'WNews', 'WIB');
@@ -50,9 +65,27 @@ VALUES ('Morgan Stanley', 2, 'NY, SFO');
 insert into company(comp_name, iid, locations)
 values ('Apple', 3, 'NY, SFO, CHI');
 
+insert into company(comp_name, iid, locations)
+values ('Dairy Queen', 4, 'NY, NJ, MA, FL');
+
 -- insert company representative into company rep table
 INSERT INTO company_rep(username,name,comp_id)
 VALUES ('lu1','Luiza', 1);
+
+insert into company_rep(username, name, comp_id)
+values ('styles', 'Harry Styles', 3);
+
+insert into company_rep(username, name, comp_id)
+values ('horan', 'Niall Horan', 1);
+
+insert into company_rep(username, name, comp_id)
+values ('louis', 'Louis Tomlinson', 2);
+
+insert into company_rep(username, name, comp_id)
+values ('liam', 'Liam Payne', 4);
+
+insert into company_rep(username, name, comp_id)
+values ('zayn', 'Zayn Malik', 4);
 
 -- insert job for lu1 into jobs table
 INSERT INTO jobs (username, title, qual1, qual2, qual3, job_status, app_link, comp_id, iid )
@@ -61,6 +94,12 @@ VALUES ('lu1','portfolio analyst', 'BA in Economics', 'Minimum GPA: 3.5',
 
 insert into jobs(username, title, qual1, qual2, qual3, job_status, app_link, comp_id, iid) 
 values ('lu1', 'software engineer', 'BA in computer engineering', 'Minimum GPA: 3.7', 'Java', 'open', NULL, 1, 1);
+
+insert into jobs(username, title, qual1, qual2, qual3, job_status, app_link, comp_id, iid) 
+values ('styles', 'Apple Music Licensing', 'MBA', NULL, 'Microsoft Office', 'open', NULL, 3, 3);
+
+insert into jobs(username, title, qual1, qual2, qual3, job_status, app_link, comp_id, iid) 
+values ('liam', 'Retail Manager', 'MBA', NULL, 'Microsoft Office', 'closed', NULL, 4, 4);
 
 -- insert experience for mars into experience table
 INSERT INTO experience (username, jid, comp_id , iid, compensation)
