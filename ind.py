094edb5826f7af0fa27592aa1c69aca6f709e01f
Nina Goodman <ninakgoodc@gmail.com>
2021-03-24 20:58:07 -0400

    new function

--- ind.py
+++ ind.py
@@ -4,11 +4,11 @@
 
 import cs304dbi as dbi 
 
-def get_industries(conn,ind_name):
+def get_companies(conn,iid):
     conn = dbi.connect()
     #Create cursor to pull data from the company table.
     curs = dbi.dict_cursor(conn)
-    curs.execute('''select industry.ind_name, industry.iid from industry where lower(industry.ind_name) like %s''',['%' + ind_name.lower() + '%'])
+    curs.execute('''select * from company where iid=%s''', [iid])
     return curs.fetchall()
 
 def get_industry(conn,iid):
@@ -18,10 +18,16 @@ def get_industry(conn,iid):
     curs.execute('''select industry.ind_name, industry.iid from industry where iid=%s''', [iid])
     return curs.fetchone()
 
-
-def get_companies(conn,iid):
+def get_industries(conn,ind_name):
     conn = dbi.connect()
     #Create cursor to pull data from the company table.
     curs = dbi.dict_cursor(conn)
-    curs.execute('''select * from company where iid=%s''', [iid])
+    curs.execute('''select industry.ind_name, industry.iid from industry where lower(industry.ind_name) like %s''',['%' + ind_name.lower() + '%'])
+    return curs.fetchall()
+
+def get_all_industries(conn):
+    conn = dbi.connect()
+    #Create cursor to pull data from the company table.
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''select iid, ind_name from industry order by ind_name asc''')
     return curs.fetchall()
