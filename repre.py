2b4342d5e10dc00ef45103793ce29dddd38a2ec0
Nina Goodman <ninakgoodc@gmail.com>
2021-03-24 20:58:39 -0400

    new function

--- repre.py
+++ repre.py
@@ -8,4 +8,9 @@ def get_rep(conn,username):
     conn = dbi.connect()
     curs = dbi.dict_cursor(conn)
     curs.execute("select name, comp_id from company_rep where username = %s", [username])
+    return curs.fetchone()
+
+def get_reps(conn,comp_id):
+    curs = dbi.dict_cursor(conn)
+    curs.execute("select * from company_rep where comp_id=%s", [comp_id])
     return curs.fetchall()
